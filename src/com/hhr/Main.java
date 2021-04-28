05fa81094268562fb86a5be254c39ded0ea86912
hhongker <hhongker@163.com>
2021-04-28 16:24:42 +0800

    sout
     Conflicts:
    	src/com/hhr/Main.java
    
     Conflicts:
    	src/com/hhr/Main.java

--- src/com/hhr/Main.java
+++ src/com/hhr/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	    // write your code here Aa100468
-        System.out.println(111);
+        System.out.println("local leavl1");
     }
 }
